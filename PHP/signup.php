021966cfa01741d7b91cf4091707a500c75cf075
priyamshah112 <priyamshah112@gmail.com>
2018-09-11 13:36:39 +0530

    done

---
+++ PHP/signup.php
@@ -0,0 +1,42 @@
+<?php 
+	if(isset($_POST['submit']))
+	{
+		$servername = "localhost";
+		$username = "root";
+		$password = "";
+		$dbname="medeasy";
+		$conn = new mysqli($servername, $username, $password, $dbname);
+
+		if ($conn->connect_error) 
+		{
+	    	die("Connection failed: " . $conn->connect_error);
+		}
+
+		$fname=mysqli_real_escape_string($conn,$_POST['ufname']);
+		$lname=mysqli_real_escape_string($conn,$_POST['ulname']);
+		$email=mysqli_real_escape_string($conn,$_POST['umail']);
+		$contact=mysqli_real_escape_string($conn,$_POST['uphone']);
+		$bdate=mysqli_real_escape_string($conn,$_POST['ubdate']);
+		$password=mysqli_real_escape_string($conn,$_POST['password']);
+		$hashpwd=password_hash($password,PASSWORD_DEFAULT);
+		$sql=mysqli_query($conn,"SELECT u_mail FROM users where u_mail='".$email."'");
+		$result = mysqli_fetch_array($sql);
+		echo $result;
+		if(empty($result)){
+			$sql2="INSERT INTO users(u_fname,u_lname,u_mail,u_password,u_phone,u_birth) VALUES ('$fname','$lname','$email','$hashpwd','$contact', '$bdate')";
+			if ($conn->query($sql2) === TRUE) {
+			    header("location:../HTML/signin.html");
+			} else {
+			    echo "Error: " . $sql2 . "<br>" . $conn->error;
+			}			
+		}
+		else{
+			echo "Username Already taken";
+		}
+
+	}
+
+	else{
+		echo "Error 404";
+	}
+?>
