7e29d6e5173ff8df14bdd7364689ef16fe06ffa0
priyamshah112 <priyamshah112@gmail.com>
2018-09-18 13:20:38 +0530

    dump1

--- PHP/login.php
+++ PHP/login.php
@@ -1,16 +1,18 @@
 <?php 
 	include_once "connection.php";
-	session_start();
 	if(isset($_POST['submit']))
 	{
 		$email=mysqli_real_escape_string($conn,$_POST['mail']);
 		$password=mysqli_real_escape_string($conn,$_POST['password']);
 		if(!empty($email) || !empty($password)){
 			if(filter_var($email,FILTER_VALIDATE_EMAIL)){
-				$sql=mysqli_query($conn,"SELECT * FROM users where u_mail='".$email."'");
-				$result = mysqli_fetch_assoc($sql);
-				if(strcmp(md5($password),$result['u_password'])){
-					header("location:../HTML/index.html?signin=success");
+				$sql=mysqli_query($conn,"SELECT * FROM users where u_mail='$email'");
+				$result = mysqli_fetch_row($sql);
+				$hashedpwd=password_verify($password,$result[6]);
+				echo "<script>console.log( 'Debug Objects: " . $result[6] . "' );</script>"; 
+				echo $hashedpwd;
+				if(strcmp(password_hash($password,PASSWORD_DEFAULT),$result[6])){
+					header("location:../HTML/index.html?signin=$hashedpwd");
 							$_SESSION['fname']=$result['u_name'];
 							$_SESSION['lname']=$result['u_lname'];
 							$_SESSION['password']=$result['u_password'];
@@ -18,9 +20,9 @@
 						    $_SESSION['phone']=$result['u_phone'];
 							$_SESSION['birth']=$result['u_birth'];
 				}
-				else{
-					header("location:../HTML/signin.html?login=invalid");
-				}
+				// elseif($hashedpwd==false){
+				// 	header("location:../HTML/signin.html?login=$a");
+				// }
 			}
 			else{
 				header("location:../HTML/signin.html?login=invalid");
