acdea0c43151c2dba4cdba85cadb930433fd7932
Devansh <devansh.solanki@somaiya.edu>
2018-10-09 15:02:02 +0530

    inventory model

---
+++ node/models/inevntoryItem.js
@@ -0,0 +1,24 @@
+var mongoose=require("mongoose");
+
+var inventoryItemSchema=new mongoose.Schema({
+    parentItem:{type:mongoose.Schema.Types.ObjectId,ref:"ParentItemSchema"},
+    inventoryItemId:{type:String},
+    mod:{type:Date},
+    expiry:{type:String},
+    status:{type:String},
+    itemImage:{type:String}
+});
+var ParentItemSchema=new mongoose.Schema({
+    parentItemId:{type:String},
+    name:{type:String},
+    quantity:{type:String},
+    cost:{type:String},
+    brand:{type:String},
+
+});
+
+var InventoryItem=mongoose.model("InventoryItem",inventoryItemSchema);
+var ParentItem=mongoose.model("ParentItem",ParentItemSchema);
+
+module.exports=InventoryItem;
+module.exports=ParentItem;
