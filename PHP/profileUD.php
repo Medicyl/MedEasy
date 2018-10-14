9bd4845a41e847d4b4ff15976050e4318015cb0b
priyamshah112 <priyamshah112@gmail.com>
2018-10-14 20:25:20 +0530

    Complete profile update

--- PHP/profileUD.php
+++ PHP/profileUD.php
@@ -1,15 +1,24 @@
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <?php
 	$link=mysqli_connect('localhost','root','');
 	mysqli_select_db($link,'medeasy');
-
+    session_start();
 		$return_arr = array();
-		echo("<script>console.log('PHP: ".$_POST['fname']." ".$_POST['lname']." ".$_POST['dob']." ".$_POST['gen']."');</script>");
+		// echo("<script>console.log('PHP: hello');</script>");
+		// echo("<script>console.log('PHP: ".$_POST['fname']." ".$_POST['lname']." ".$_POST['dob']." ".$_POST['gen']."');</script>");
+		$email = $_POST['email'];
+		$fname = $_POST['fname'];
+		$lname = $_POST['lname'];
+		$dob   = $_POST['dob'];
+		$gen   = $_POST['gen'];
 
-		$query1 = $link->query("UPDATE users SET u_fname = ".$_POST['fname'].",u_lname = ".$_POST['lname'].",u_birth = ".$_POST['dob'].",u_gender = ".$_POST['gen']." WHERE u_mail = ".$_POST['email']."");
-
-		$query = $link->query("SELECT * FROM users WHERE u_mail = ".$_POST['email']."");
+		$query1 = $link->query("UPDATE users SET u_fname='$fname',u_lname='$lname',u_birth='$dob',u_gender='$gen' WHERE u_mail='$email'");
+		 // Test of Updaataion
+            // if(!$query1 ) {
+            //    die('Could not update data: ' . mysql_error());
+            // }
+            // echo "Updated data successfully\n";
+		$query = $link->query("SELECT * FROM users WHERE u_mail ='$email'");
 
 		    // //Count total number of rows
 		    $rowCount = $query->num_rows;
@@ -23,7 +32,11 @@
 				    $lname = $row['u_lname'];
 				    $dob = $row['u_birth'];
 				    $gen = $row['u_gender'];
-
+				    // echo $fname." ".$lname." ".$dob." ".$gen;
+				    $_SESSION['fname'] = $fname;
+				    $_SESSION['lname'] = $lname;
+				    $_SESSION['birth'] = $dob;
+				    $_SESSION['gen'] = $gen;
 				    $return_arr[] = array("fname" => $fname,
 				                    "lname" => $lname,
 				                    "dob" => $dob,
